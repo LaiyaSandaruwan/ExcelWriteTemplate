63ebcef817cda9901177ecae08bbd1db12184060
Lahiru Sandaruwan <l.sandaruwan@hayleysadvantis.com>
2024-04-09 07:32:13 +0530

    project setup

---
+++ ExcelWriteTest/ExcelWriteTest/Form1.Designer.vb
@@ -0,0 +1,65 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnExport = New System.Windows.Forms.Button()
+        Me.dgvIssuing = New System.Windows.Forms.DataGridView()
+        CType(Me.dgvIssuing, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnExport
+        '
+        Me.btnExport.Location = New System.Drawing.Point(1078, 608)
+        Me.btnExport.Name = "btnExport"
+        Me.btnExport.Size = New System.Drawing.Size(75, 23)
+        Me.btnExport.TabIndex = 0
+        Me.btnExport.Text = "Export"
+        Me.btnExport.UseVisualStyleBackColor = True
+        '
+        'dgvIssuing
+        '
+        Me.dgvIssuing.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvIssuing.Location = New System.Drawing.Point(12, 12)
+        Me.dgvIssuing.Name = "dgvIssuing"
+        Me.dgvIssuing.RowHeadersWidth = 51
+        Me.dgvIssuing.RowTemplate.Height = 24
+        Me.dgvIssuing.Size = New System.Drawing.Size(1166, 590)
+        Me.dgvIssuing.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1190, 643)
+        Me.Controls.Add(Me.dgvIssuing)
+        Me.Controls.Add(Me.btnExport)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dgvIssuing, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnExport As Button
+    Friend WithEvents dgvIssuing As DataGridView
+End Class
---
+++ ExcelWriteTest/ExcelWriteTest/Form1.vb
@@ -0,0 +1,55 @@
+﻿Imports System.IO
+Imports OfficeOpenXml
+
+Public Class Form1
+    Private Sub btnExport_Click(sender As Object, e As EventArgs) Handles btnExport.Click
+        Dim folderBrowserDialog As New FolderBrowserDialog()
+        folderBrowserDialog.SelectedPath = "C:\"
+
+        If folderBrowserDialog.ShowDialog() = DialogResult.OK Then
+            Dim selectedFolderPath As String = folderBrowserDialog.SelectedPath
+
+            Dim fileName As String = "MyExcelFile.xlsx"
+            Dim savePath As String = Path.Combine(selectedFolderPath, fileName)
+
+            Try
+                Dim fileInfo = New FileInfo(savePath)
+                Using excelPackage As ExcelPackage = New ExcelPackage(fileInfo)
+                    Dim worksheet As ExcelWorksheet = excelPackage.Workbook.Worksheets.Add("Suppliers")
+                    worksheet.Cells.LoadFromCollection(Of IssuingDet)(TryCast(dgvIssuing.DataSource, List(Of IssuingDet)), True)
+                    excelPackage.Save()
+                    MessageBox.Show("Saved")
+                End Using
+            Catch ex As Exception
+                MessageBox.Show(ex.Message)
+            End Try
+        End If
+
+        'Using sfd As SaveFileDialog = New SaveFileDialog() With {.Filter = "Excel Workbook|*.xlsx"}
+        '    Try
+        '        ' Set the default file name
+        '        sfd.FileName = "MyExcelFile.xlsx"
+
+        '        If sfd.ShowDialog() = DialogResult.OK Then
+        '            Dim fileInfo = New FileInfo(sfd.FileName)
+        '            Using excelPackage As ExcelPackage = New ExcelPackage(fileInfo)
+        '                Dim worksheet As ExcelWorksheet = excelPackage.Workbook.Worksheets.Add("Suppliers")
+        '                worksheet.Cells.LoadFromCollection(Of IssuingDet)(TryCast(dgvIssuing.DataSource, List(Of IssuingDet)), True)
+        '                excelPackage.Save()
+        '                MessageBox.Show("Saved")
+        '            End Using
+        '        End If
+        '    Catch ex As Exception
+        '        MessageBox.Show(ex.Message)
+        '    End Try
+        'End Using
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Using db As LogixFMEntities = New LogixFMEntities()
+            dgvIssuing.DataSource = (From IH In db.IssuingDets
+                                     Where IH.numIssID = 118
+                                     Select IH).ToList()
+        End Using
+    End Sub
+End Class
